d820d528f539177d52df47f613e97926b5bd0c44
Joe <53659792+joez-zh@users.noreply.github.com>
2025-06-01 15:07:43 -0400

    added more javadocs and comments

--- GreenBar.java
+++ GreenBar.java
@@ -1,27 +1,48 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GreenBar here.
+ * Green bar - used for health bars for enemies
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Joe and Kelton
+ * @version June 2025
  */
 public class GreenBar extends SmoothMover
 {
     private GreenfootImage image;
     private int startWidth;
     
+    /**
+     * Constructor for a green bar
+     * 
+     * @param xScale: horizontal scale
+     * @param yScale: vertical scale
+     */
     public GreenBar(double xScale, double yScale) {
+        // sets the image
         image = new GreenfootImage("greenBar.png");
         setImage(image);
+        
+        // scales the image
         image.scale((int)(image.getWidth() * xScale), (int)(image.getHeight() * yScale));
         
         startWidth = image.getWidth();
     }
     
+    /**
+     * Changes the position and percentage of the green bar
+     * 
+     * @param x: new x location
+     * @param y: new y location
+     * @param percentage: new percentage of the bar
+     */
     public void setPos(double x, double y, double percentage) {
+        // removes if the percentage reaches 0
         if (percentage <= 0) return;
+        
+        // changes location
         setLocation(x - ((int)startWidth*(1-percentage)/2), y + 20);
+        
+        // new scale
         image.scale(Math.max(1, (int)(startWidth*percentage)), image.getHeight());
     }
 }
