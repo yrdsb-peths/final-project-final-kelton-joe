2c1084050a4d046cd15391db5be72573e35e07d2
Kelton <keltonkuan@gmail.com>
2025-06-05 09:49:15 -0400

    Fixed vampire heal

--- Enemy.java
+++ Enemy.java
@@ -304,6 +304,8 @@ public class Enemy extends SmoothMover
         if (Hero.hero.vampireLvl == 1) {
             // increases current hp by 5% of max hp
             Hero.hero.currentHp = Math.min((int) (Hero.hero.currentHp + (0.05 * Hero.hero.maxHp)), Hero.hero.maxHp);
+            // updates health bar
+            GameWorld.healthBar.setValue(Math.max(Hero.hero.currentHp, 0) + "/" + Hero.hero.maxHp + " hp");
         }
         else if (Hero.hero.vampireLvl >= 2) {
             // chance to grant additional hp on kill (33%)
